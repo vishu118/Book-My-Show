d18c5f3b161f54175e9180f628fe336333adca6b
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-05-26 12:57:23 +0530

    cities in navbar

---
+++ JS/Navbar.js
@@ -0,0 +1,6 @@
+const cityOption = document.querySelectorAll('.right_nav_city')
+
+cityOption.addEventListener("click",()=>{
+    cityPopUpContainer.classList.add("display_block");
+    cityPopUp.classList.add("display_block");
+ })
