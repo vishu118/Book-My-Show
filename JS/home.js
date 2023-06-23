d8264b655db0eab6cf6fc83f56abe3dbd2f10ae7
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-06-23 13:59:41 +0530

    .

--- JS/home.js
+++ JS/home.js
@@ -37,32 +37,34 @@ const popularMoviesUrl2 = Base_Url + "discover/movie?" + Api_key + "&sort_by=pop
 
 
 
-// async function getMovies(url) {
-//     const fetchedData = await fetch(url, {
-//         method: "GET",
-//         headers: {
-//             Accept: "application/json",
-//             Authorization: auth
-//         }
-//     })
-//     const data = await fetchedData.json();
-//     showMovies(data.results); 
-// }
-// getMovies(recommendedMoviesUrl);
-
-
-
-
-
-const getMovies = async(url) =>{
-    const fetchedData = await fetch(url)
-    console.log(fetchedData)
+async function getMovies(url) {
+    const fetchedData = await fetch(url, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            Authorization: auth
+        }
+    })
     const data = await fetchedData.json();
-    console.log(data.results)
     showMovies(data.results); 
 }
-
+getMovies(recommendedMoviesUrl);
 getMovies(latestMoviesUrl);
+getMovies(likedIndianMovies);
+
+
+
+
+
+// const getMovies = async(url) =>{
+//     const fetchedData = await fetch(url)
+//     console.log(fetchedData)
+//     const data = await fetchedData.json();
+//     console.log(data.results)
+//     showMovies(data.results); 
+// }
+
+// getMovies(latestMoviesUrl);
 
 
 function mapMovieLanguage(lang) {
@@ -81,7 +83,7 @@ function mapMovieLanguage(lang) {
 }
 
 function createMovieElement(movie){
-    let { title, poster_path, vote_average, popularity, original_language, id } = movie;
+    let { title, poster_path, vote_average, popularity, original_language, id,genresOfMovie} = movie;
     original_language = mapMovieLanguage(original_language);
 
     const movieEl = document.createElement("div")
@@ -101,6 +103,260 @@ function createMovieElement(movie){
     return movieEl;
 }
 
+
+
+function showMovies(movies) {
+  
+    movies.forEach((movie) => {
+      
+      let { title, poster_path, vote_average, overview, popularity, original_language,id, genre_ids } = movie
+       original_language = mapMovieLanguage(original_language);
+
+        const movieEl = document.createElement("div")
+        let genres = ["Action/Mystery","Comedy/Drama/Romance","Action/Thriller","Drama/Mystery"]
+        let rndm = Math.floor(Math.random() *genres.length)
+        let genresOfMovie = genres[rndm]
+        movieEl.classList.add("movies")
+        movieEl.innerHTML = `
+                 <div class="parent_movie">
+                 <a href="./HTML/movieExpanded.html?id=${id}"><img src="${img_url + poster_path}" alt="" /></a>
+                 <div class="like_vote">
+                 <p><i class="fa-solid fa-thumbs-up"></i> ${Number(popularity/10).toFixed(1)}k likes</p>
+                 <p><i class="fa-solid fa-star"></i> ${vote_average}/10</p>
+                 </div>
+                 </div>
+                 <h2 style="color: white;font-weight: 100;">${title}</h2>
+                 <p style="color: white;font-weight: 100;">${original_language}</p>
+              `
+                 recommendedMovieContainerEl.appendChild(movieEl)
+ })
+
+}
+
+
+
+
+// const getlikedIndianMovies = async function (url) {
+
+//     const fetchedDataOfLikedIndianMovies = await fetch(url, {
+//         method: "GET",
+//         headers: {
+//             Accept: "application/json",
+//             Authorization: auth
+
+//         }
+//     })
+//     const data = await fetchedDataOfLikedIndianMovies.json();
+//     console.log(data.results)
+//     showLikedIndianMovies(data.results)
+// }
+
+
+// getlikedIndianMovies(likedIndianMovies)
+
+// function showLikedIndianMovies(movies) {
+//     console.log(movies)
+
+//     movies.forEach((movie) => {
+//         let movieEl = createMovieElement(movie);
+//         indianMoviesContainerEl.appendChild(movieEl);
+//     });
+// }
+
+
+
+
+
+
+
+
+
+
+
+//=================================================================================================================================================================================================//
+
+// //Upcoming Movies
+
+
+
+
+// const getlikedIndianMovies = async function (url) {
+
+//     const fetchedDataOfLikedIndianMovies = await fetch(url, {
+//         method: "GET",
+//         headers: {
+//             Accept: "application/json",
+//             Authorization: auth
+
+//         }
+//     })
+//     const data = await fetchedDataOfLikedIndianMovies.json();
+//     console.log(data.results)
+//     showLikedIndianMovies(data.results)
+// }
+
+
+// getlikedIndianMovies(likedIndianMovies)
+
+// function showLikedIndianMovies(movies) {
+//     console.log(movies)
+
+//     movies.forEach((movie) => {
+//         let movieEl = createMovieElement(movie);
+//         indianMoviesContainerEl.appendChild(movieEl);
+//     });
+// }
+
+
+
+//===================================================================================================================================================================================================//
+
+
+// //Premering Movies 
+
+// const getNowPlayingMovies = async function (url) {
+
+//     const fetchedDataOfNowPlayingMovies = await fetch(url, {
+//         method: "GET",
+//         headers: {
+//             Accept: "application/json",
+//             Authorization: auth
+//         }
+//     })
+//     const data = await fetchedDataOfNowPlayingMovies.json();
+//     console.log(data.results)
+//     showNowPlayingMovies(data.results)
+// }
+// getNowPlayingMovies(nowPlayingMoviesUrl)
+
+// function showNowPlayingMovies(movies) {
+//     console.log(movies)
+
+//     movies.forEach((movie) => {
+//         let movieEl = createMovieElement(movie);
+//         nowPlayingMoviesContainerEl.appendChild(movieEl);
+//     });
+// }
+
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+
+// popular
+
+
+// const getPopularMovies = async function (url) {
+
+// //     const fetchedDataOfGetPopularMovies = await fetch(url, {
+// //         method: "GET",
+// //         headers: {
+// //             Accept: "application/json",
+// //             Authorization: auth
+// //         }
+// //     })
+// //     const data = await fetchedDataOfGetPopularMovies.json();
+// //     console.log(data.results);
+// //     showPopularMovies(data.results)
+// // }
+// // getPopularMovies(popularMoviesUrl2)
+
+// // function showPopularMovies(movies) {
+// //     console.log(movies)
+
+// //     movies.forEach((movie) => {
+// //         let movieEl = createMovieElement(movie);
+// //         popularMoviesContainerEl.appendChild(movieEl);
+// //     });
+// // }
+
+// //----------------------------------------------------------------------
+
+// //Top Rated
+
+// // const getTopRatedMovies = async function (url) {
+
+// //     const fetchedDataOfTopRatedMovies = await fetch(url, {
+// //         method: "GET",
+// //         headers: {
+// //             Accept: "application/json",
+// //             Authorization: auth
+// //         }
+// //     })
+// //     const data = await fetchedDataOfTopRatedMovies.json();
+// //     console.log(data.results);
+// //     showTopRatedMovies(data.results)
+// // }
+// // getTopRatedMovies(topRatedMoviesUrl)
+
+// // function showTopRatedMovies(movies) {
+// //     console.log(movies)
+
+// //     movies.forEach((movie) => {
+// //         let movieEl = createMovieElement(movie);
+// //         topRatedMoviesContainerEl.appendChild(movieEl);
+// //     });
+// // }
+
+// //-------------------------------------------------------------------------------------------------------------
+
+// //Latest Movies
+
+
+// // const getLatestMovies = async function (url) {
+
+// //     const fetchedDataOfLatestMovies = await fetch(url, {
+// //         method: "GET",
+// //         headers: {
+// //             Accept: "application/json",
+// //             Authorization: auth
+// //         }
+// //     })
+// //     const data = await fetchedDataOfLatestMovies.json();
+// //     console.log(data.results);
+// //     showLatestMovies(data.results)
+// // }
+// // getLatestMovies(latestMoviesUrl);
+// // function showLatestMovies(movies) {
+// //     console.log(movies)
+
+// //     movies.forEach((movie) => {
+// //         let movieEl = createMovieElement(movie);
+// //         latestMoviesContainerEl.appendChild(movieEl);
+// //     });
+// // }
+// //------------------------------------------------------------------------------------------------------------------------------
+
+// //Trending Searches
+
+// // const getTrendingMovies = async function (url) {
+
+// //     const fetchedDataOfTrendingMovies = await fetch(url, {
+// //         method: "GET",
+// //         headers: {
+// //             Accept: "application/json",
+// //             Authorization: auth
+// //         }
+// //     })
+// //     const data = await fetchedDataOfTrendingMovies.json();
+// //     console.log(data.results);
+// //     showTrendingMovies(data.results.slice(3, 17));
+// // }
+// // getTrendingMovies(trendingMovieNamesUrl);
+
+
+// // function showTrendingMovies(movies) {
+// //     movies.forEach(movie => {
+// //         let { id, title } = movie;
+// //         const trendingMoviesEl = document.createElement("div")
+// //         trendingMoviesEl.classList.add("trending_movies")
+// //         trendingMoviesEl.innerHTML = `
+// //           <a href="./HTML/movieExpanded.html?id=${id}">
+// //               <p>${title}</p>
+// //           </a>
+        
+// //         `
+// //         trendingMoviesContainerEl.appendChild(trendingMoviesEl)
+// //     })
+// // }
 
 
 
@@ -159,73 +415,73 @@ function createMovieElement(movie){
 // getMovies(recommendedMoviesUrl);
 
 
-function mapMovieLanguage(lang) {
+// function mapMovieLanguage(lang) {
 
-    let movieLangObj = {
-        "en": "English",
-        "hi": "Hindi",
-        "te": "Telegu",
-        "ta": "Tamil",
-        "ml": "Malayalam",
-        "bn": "Bengali"
-    }
+//     let movieLangObj = {
+//         "en": "English",
+//         "hi": "Hindi",
+//         "te": "Telegu",
+//         "ta": "Tamil",
+//         "ml": "Malayalam",
+//         "bn": "Bengali"
+//     }
 
-    return movieLangObj[lang];
+//     return movieLangObj[lang];
 
-}
+// }
 
-function createMovieElement(movie) {
-    let { title, poster_path, vote_average, popularity, original_language, id } = movie;
-    original_language = mapMovieLanguage(original_language);
+// function createMovieElement(movie) {
+//     let { title, poster_path, vote_average, popularity, original_language, id } = movie;
+//     original_language = mapMovieLanguage(original_language);
 
-    const movieEl = document.createElement("div")
-    let genres = ["Action/Mystery", "Comedy/Drama/Romance", "Action/Thriller", "Drama/Mystery"]
-    let rndm = Math.floor(Math.random() * genres.length)
-    let genresOfMovie = genres[rndm]
-    movieEl.classList.add("movies")
-    movieEl.innerHTML = `
-    <div class="parent_movie"> 
-    <a href="./HTML/movieExpanded.html?id=${id}"><img src="${img_url + poster_path}" alt="" /></a>
-    <div class="like_vote">
-    <p><i class="fa-solid fa-thumbs-up"></i> ${Number(popularity / 10).toFixed(1)}k likes</p>
-    <p><i class="fa-solid fa-star"></i> ${vote_average}/10</p>
-    </div>
-    </div>
-    <h2>${title}</h2>
-    <p>${genresOfMovie}</p>
-    `;
+//     const movieEl = document.createElement("div")
+//     let genres = ["Action/Mystery", "Comedy/Drama/Romance", "Action/Thriller", "Drama/Mystery"]
+//     let rndm = Math.floor(Math.random() * genres.length)
+//     let genresOfMovie = genres[rndm]
+//     movieEl.classList.add("movies")
+//     movieEl.innerHTML = `
+//     <div class="parent_movie"> 
+//     <a href="./HTML/movieExpanded.html?id=${id}"><img src="${img_url + poster_path}" alt="" /></a>
+//     <div class="like_vote">
+//     <p><i class="fa-solid fa-thumbs-up"></i> ${Number(popularity / 10).toFixed(1)}k likes</p>
+//     <p><i class="fa-solid fa-star"></i> ${vote_average}/10</p>
+//     </div>
+//     </div>
+//     <h2>${title}</h2>
+//     <p>${genresOfMovie}</p>
+//     `;
 
-    return movieEl;
-}
+//     return movieEl;
+// }
 
 
-function showMovies(movies) {
+// function showMovies(movies) {
   
-    movies.forEach((movie) => {
+//     movies.forEach((movie) => {
       
-      let { title, poster_path, vote_average, overview, popularity, original_language,id, genre_ids } = movie
-       original_language = mapMovieLanguage(original_language);
+//       let { title, poster_path, vote_average, overview, popularity, original_language,id, genre_ids } = movie
+//        original_language = mapMovieLanguage(original_language);
 
-        const movieEl = document.createElement("div")
-        let genres = ["Action/Mystery","Comedy/Drama/Romance","Action/Thriller","Drama/Mystery"]
-        let rndm = Math.floor(Math.random() *genres.length)
-        let genresOfMovie = genres[rndm]
-        movieEl.classList.add("movies")
-        movieEl.innerHTML = `
-                 <div class="parent_movie">
-                 <a href="./HTML/movieExpanded.html?id=${id}"><img src="${img_url + poster_path}" alt="" /></a>
-                 <div class="like_vote">
-                 <p><i class="fa-solid fa-thumbs-up"></i> ${Number(popularity/10).toFixed(1)}k likes</p>
-                 <p><i class="fa-solid fa-star"></i> ${vote_average}/10</p>
-                 </div>
-                 </div>
-                 <h2 style="color: white;font-weight: 100;">${title}</h2>
-                 <p style="color: white;font-weight: 100;">${original_language}</p>
-              `
-                 recommendedMovieContainerEl.appendChild(movieEl)
- })
+//         const movieEl = document.createElement("div")
+//         let genres = ["Action/Mystery","Comedy/Drama/Romance","Action/Thriller","Drama/Mystery"]
+//         let rndm = Math.floor(Math.random() *genres.length)
+//         let genresOfMovie = genres[rndm]
+//         movieEl.classList.add("movies")
+//         movieEl.innerHTML = `
+//                  <div class="parent_movie">
+//                  <a href="./HTML/movieExpanded.html?id=${id}"><img src="${img_url + poster_path}" alt="" /></a>
+//                  <div class="like_vote">
+//                  <p><i class="fa-solid fa-thumbs-up"></i> ${Number(popularity/10).toFixed(1)}k likes</p>
+//                  <p><i class="fa-solid fa-star"></i> ${vote_average}/10</p>
+//                  </div>
+//                  </div>
+//                  <h2 style="color: white;font-weight: 100;">${title}</h2>
+//                  <p style="color: white;font-weight: 100;">${original_language}</p>
+//               `
+//                  recommendedMovieContainerEl.appendChild(movieEl)
+//  })
 
-}
+// }
 
 //=================================================================================================================================================================================================//
 
