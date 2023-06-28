c4eff0e49d6c9f97c5c5455b5cfc667157738a0d
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-06-28 09:04:23 +0530

    working on getting movie details on theatre page

--- JS/Theatre.js
+++ JS/Theatre.js
@@ -1,6 +1,50 @@
+const id = new URLSearchParams(window.location.search).get("id");
 
-import { getFooter2 } from '../MODULES/footer2Module';
+const bookingCont = document.querySelector(".theatre_location");
+const workshopContainer = document.querySelector(".workshop");
+const booking_head_el = document.querySelector(".booking_head");
+const booking_filter_el = document.querySelector(".booking_filter");
+let seatsToBookEl = document.querySelector(".seatstobook");
+const datesContainerEl = document.querySelector(".dates");
+
+let Api_key = "api_key=2e302e23979f60ced7d629e4168670c9";
+let Base_Url = "https://api.themoviedb.org/3/";
+let getMovieDetails = `${Base_Url}movie/${id}?${Api_key}`;
+
+const renderDetails = async () => {
+  const res = await fetch(getMovieDetails);
+  const data = res.json();
+  console.log(data)
+  const { original_title, genres, spoken_languages, adult } = data;
+
+  const template = `
+    <div class="booking_title container">
+    <p class="movie_title"><a href="../HTML/movieExpanded.html?id=${id}"> <i class="fa-solid fa-less-than movie_back" ></i></a> <span> ${original_title} -${spoken_languages[0].english_name}</span></p>
+    </div>
+    <div class="booking_genere container">
+    <i class="fa-brands fa-umbraco"></i>
+    <span class="genere">${genres[0].name}</span>
+    <span class="genere">${genres[1].name}</span>
+    <span class="genere">${genres[2].name}</span>
+  </div>
+    
+    
+    `
+    booking_head_el.innerHTML = template
 
 
-document.querySelector('.m_footer2').innerHTML = getFooter2();
 
+    const template2 = `
+    <div class="booking_filter">
+    <p class="moviefilter">${spoken_languages[0].english_name}-2D</p>
+   
+  </div>
+    `
+
+    booking_filter_el.innerHTML = template2
+
+   
+};
+
+
+window.addEventListener("DOMContentLoaded", () => renderDetails())
